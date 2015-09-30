36084e55b922e98027a738ec2fecd69bea59e2ea
Patrick Bergman <p.m.bergman@umail.leidenuniv.nl>
2015-09-30 12:03:26 +0200

    Initial Commit

---
+++ Trees/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Trees
+//
+//  Created by Patrick Bergman on 30/09/15.
+//  Copyright © 2015 Patrick Bergman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
